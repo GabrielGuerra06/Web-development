de615e4c9bd0b37cbaf1128b03fa4e2cc20cd9df
Gabriel Guerra Rosales <97709500+GabrielGuerra06@users.noreply.github.com>
2022-10-06 20:00:27 -0500

    Add files via upload

---
+++ Node JS/Node 5/index.js
@@ -0,0 +1,7 @@
+var sv= require("supervillains")
+var sh = require("superheroes")
+
+var randomVillain = sv.random()
+var randomHeroe = sh.random()
+
+console.log("Be careful everyone,  "+ randomVillain + " wants to destroy all the city we are lucky to have " + randomHeroe + " to save the city, come on everyone this its gonna be the fight of the century!! " + randomVillain + " Vs. " + randomHeroe)
